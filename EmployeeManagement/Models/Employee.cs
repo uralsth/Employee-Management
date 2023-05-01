e41cd5f9348c1c153560d47d787f7d5b9c1b81bd
Ural Shrestha <shresthaural100@gmail.com>
2023-05-01 20:13:08 +0545

    Done model valdation

--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -1,10 +1,18 @@
-﻿namespace EmployeeManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage ="Name cannot exceed 50 characters")]
         public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid email format")]
+        [Display(Name = "Office Email")]
         public string Email { get; set; }
-        public string Department { get; set; }
+        [Required]
+        public Dept? Department { get; set; }
     }
 }

5477b21726fab0fe205ca74d098ed0d4b23a90c8
Ural Shrestha <shresthaural100@gmail.com>
2023-05-01 20:14:55 +0545

    implemented add method from interface

--- EmployeeManagement/Models/MockEmployeeRepository.cs
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -9,11 +9,18 @@ namespace EmployeeManagement.Models
         {
             _employeeList = new List<Employee>()
             {
-                new Employee() {Id = 1, Name = "Ural", Department = "IT", Email = "shresthaural100@gmai.com"},
-                new Employee() {Id = 2, Name = "Sudan", Department = "HR", Email = "shresthasudan100@gmai.com"},
-                new Employee() {Id = 3, Name = "Rajish", Department = "IT", Email = "shrestharajish100@gmai.com"}
+                new Employee() {Id = 1, Name = "Ural", Department =Dept.IT, Email = "shresthaural100@gmai.com"},
+                new Employee() {Id = 2, Name = "Sudan", Department =Dept.Hr, Email = "shresthasudan100@gmai.com"},
+                new Employee() {Id = 3, Name = "Rajish", Department =Dept.Payroll, Email = "shrestharajish100@gmai.com"}
             };
          }
+
+        public Employee Add(Employee employee)
+        {
+            employee.Id = _employeeList.Max(e => e.Id) + 1;
+            _employeeList.Add(employee);
+            return employee;
+        }
 
         public IEnumerable<Employee> GetAllEmployeee()
         {

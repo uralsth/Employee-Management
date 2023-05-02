c2d77dc70e6df1d3c09bdc8d422a416ffd68da1e
Ural Shrestha <shresthaural100@gmail.com>
2023-05-02 12:34:36 +0545

    extension class of ModelBuilder for seeding

---
+++ EmployeeManagement/Models/ModelBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Models
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Mary",
+                    Department = Dept.IT,
+                    Email = "mary@gmail.com"
+                },
+
+                new Employee
+                {
+                    Id = 2,
+                    Name = "John",
+                    Department = Dept.Hr,
+                    Email = "John@gmail.com"
+                }
+                );
+        }
+    }
+}

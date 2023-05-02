0c2f4bcd6279adb937096ceaf110ffc3e7efb8d6
Ural Shrestha <shresthaural100@gmail.com>
2023-05-02 12:33:43 +0545

    create a class that derived from DbContext to interact with database

---
+++ EmployeeManagement/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Seed();
+        }
+    }
+}

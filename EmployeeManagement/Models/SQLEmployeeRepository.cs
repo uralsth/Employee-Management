b78f78ccd878915a9d20bbee06511b40d59460e6
Ural Shrestha <shresthaural100@gmail.com>
2023-05-02 12:35:14 +0545

    added implementation of operation using EF core

---
+++ EmployeeManagement/Models/SQLEmployeeRepository.cs
@@ -0,0 +1,47 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class SQLEmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext context;
+
+        public SQLEmployeeRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public Employee Add(Employee employee)
+        {
+            context.Add(employee);
+            context.SaveChanges();
+            return employee;
+        }
+
+        public Employee Delete(int id)
+        {
+            Employee employee = context.Employees.Find(id);
+            if (employee != null)
+            {
+                context.Employees.Remove(employee);
+                context.SaveChanges();
+            }
+            return employee;
+        }
+
+        public IEnumerable<Employee> GetAllEmployeee()
+        {
+            return context.Employees;
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return context.Employees.Find(Id);
+        }
+
+        public Employee Update(Employee employeeChanges)
+        {
+            var employee = context.Employees.Attach(employeeChanges);
+            employee.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            context.SaveChanges();
+            return employeeChanges;
+        }
+    }
+}

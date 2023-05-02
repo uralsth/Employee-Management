224b285406e38648a5d2aba42fc07076fb6fb6e7
Ural Shrestha <shresthaural100@gmail.com>
2023-05-02 12:32:09 +0545

    used Sql employee repository instead of In memmory repository

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,29 +1,31 @@
 using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeManagement
 {
     public class Program
     {
-        public class TestModel
+        public class ConfigurationManager
         {
             private readonly IConfiguration _config;
-            public TestModel(IConfiguration config)
+            public ConfigurationManager(IConfiguration config)
             {
                 _config = config;
             }
 
-            public static string getValue (IConfiguration config)
+            public static string? getConnectionString (IConfiguration config)
             {
-                string value = config["MyKey"];
+                string? value = config.GetConnectionString("EmployeeDBConnection");
                 return value;
             }
         }
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(ConfigurationManager.getConnectionString(builder.Configuration)));
             builder.Services.AddControllersWithViews();
             builder.Services.AddRouting();
-            builder.Services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+            builder.Services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
             var app = builder.Build();
 
             //FileServerOptions fileServerOptions = new FileServerOptions();

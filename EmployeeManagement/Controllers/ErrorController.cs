69f99ce28772ef140494277779a030a065fe5c84
Ural Shrestha <shresthaural100@gmail.com>
2023-05-11 13:22:59 +0545

    custom logging in a file using NLog

---
+++ EmployeeManagement/Controllers/ErrorController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+	public class ErrorController : Controller
+	{
+		private readonly ILogger<ErrorController> logger;
+
+		public ErrorController(ILogger<ErrorController> logger)
+        {
+			this.logger = logger;
+		}
+        [Route("Error/{statusCode}")]
+		public IActionResult HttpStatusCodeHandler(int statusCode)
+		{
+			var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+			switch (statusCode)
+			{
+				case 404:
+					ViewBag.ErrorMessage = "Sorry, the resource you request could not be found";
+					logger.LogWarning($"404 error occured. Path={statusCodeResult.OriginalPath}" + $"and QueryString = {statusCodeResult.OriginalQueryString}");
+					//ViewBag.Path = statusCodeResult.OriginalPath;
+					//ViewBag.QS = statusCodeResult.OriginalQueryString;
+					break;
+			}
+			return View("NotFound");
+		}
+
+		[Route("Error")]
+		[AllowAnonymous]
+		public IActionResult Error()
+		{
+			var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+			//ViewBag.ExceptionPath = exceptionDetails.Path;
+			//ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
+			//ViewBag.Stacktrace = exceptionDetails.Error.StackTrace;
+
+			logger.LogError($"The path {exceptionDetails.Path} threw and exception {exceptionDetails.Error}");
+
+			return View("Error");
+		}
+	}
+}
--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -1,71 +1,155 @@
 ﻿using EmployeeManagement.Models;
 using EmployeeManagement.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using System.ComponentModel.DataAnnotations;
+using EmployeeManagement.Migrations;
 
 namespace EmployeeManagement.Controllers
 {
-    //public class HomeController : Controller
-    //{
-    //    public IActionResult Index()
-    //    {
-    //        return View();
-    //    }
-    //}
+	//public class HomeController : Controller
+	//{
+	//    public IActionResult Index()
+	//    {
+	//        return View();
+	//    }
+	//}
 
-    public class HomeController : Controller
-    {
-        private readonly IEmployeeRepository _employeeRepository;
+	public class HomeController : Controller
+	{
+		private readonly IEmployeeRepository _employeeRepository;
+		private readonly IWebHostEnvironment webHostEnvironment;
 
-        public HomeController(IEmployeeRepository employeeRepository)
-        {
-            _employeeRepository = employeeRepository;
-        }
-        public ViewResult Index()
-        {
-            var model = _employeeRepository.GetAllEmployeee();
-            return View(model);
-        }
+		public HomeController(IEmployeeRepository employeeRepository, IWebHostEnvironment webHostEnvironment)
+		{
+			_employeeRepository = employeeRepository;
+			this.webHostEnvironment = webHostEnvironment;
+		}
+		public ViewResult Index()
+		{
+			var model = _employeeRepository.GetAllEmployeee();
+			return View(model);
+		}
 
-        public ViewResult Details()
-        {
+		public ViewResult Details(int? id)
+		{
 
-            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
-            {
-                Employee = _employeeRepository.GetEmployee(1),
-                PageTitle = "Employee Details"
-            };
+			//throw new Exception("Excetpiton error");
+			Employee employee = _employeeRepository.GetEmployee(id.Value);
 
-            //Employee model = _employeeRepository.GetEmployee(1);
+			if(employee == null)
+			{
+				Response.StatusCode = 404;
+				return View("EmployeeNotFound", id.Value);
+			}
 
-            //ViewData["Employee"] = model;
-            //ViewData["PageTitle"] = "Employee Details";
+			HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+			{
+				Employee = employee,
+				PageTitle = "Employee Details"
+			};
 
-            //ViewBag.Employee = model;
-            //ViewBag.PageTitle = "Employee Details";
+			//Employee model = _employeeRepository.GetEmployee(1);
 
-            return View(homeDetailsViewModel);
+			//ViewData["Employee"] = model;
+			//ViewData["PageTitle"] = "Employee Details";
 
-            //return View("Test");
-            //return View("MyViews/Tes.cshtml");
-        }
+			//ViewBag.Employee = model;
+			//ViewBag.PageTitle = "Employee Details";
 
-        [HttpGet]
-        public ViewResult Create()
-        {
-            return View();
-        }
+			return View(homeDetailsViewModel);
 
-        [HttpPost]
-        public IActionResult Create(Employee employee)
-        {
-            if (ModelState.IsValid)
-            {
-                Employee newEmployee = _employeeRepository.Add(employee);
-                //return RedirectToAction("details", new { id = newEmployee.Id });
-            }
-            return View();
-        }
+			//return View("Test");
+			//return View("MyViews/Tes.cshtml");
+		}
 
-    }
+		[HttpGet]
+		public ViewResult Create()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public ViewResult Edit(int id)
+		{
+			Employee employee = _employeeRepository.GetEmployee(id);
+			EmployeeEditViewModel employeeEditViewModel = new EmployeeEditViewModel
+			{
+				Id = employee.Id,
+				Name = employee.Name,
+				Email = employee.Email,
+				Department = employee.Department,
+				ExistingPhotoPath = employee.PhotoPath
+			};
+			return View(employeeEditViewModel);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(EmployeeEditViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				Employee employee = _employeeRepository.GetEmployee(model.Id);
+				employee.Name = model.Name;
+				employee.Email = model.Email;
+				employee.Department = model.Department;
+				if (model.Photos.Count > 0 && model.Photos != null)
+				{
+					if(model.ExistingPhotoPath != null)
+					{
+						string filePath = Path.Combine(webHostEnvironment.WebRootPath, "images", model.ExistingPhotoPath);
+						System.IO.File.Delete(filePath);
+					}
+
+					employee.PhotoPath = ProcessUploadedFile(model);
+				}
+
+				_employeeRepository.Update(employee);
+				return RedirectToAction("index");
+			}
+			return View();
+		}
+
+		private string ProcessUploadedFile(EmployeeCreateViewModel model)
+		{
+			string uniqueFileName = null;
+			if (model.Photos != null && model.Photos.Count > 0)
+			{
+				foreach (IFormFile photo in model.Photos)
+				{
+					string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+					uniqueFileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
+					string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+					using (var filestream = new FileStream(filePath, FileMode.Create))
+					{
+						photo.CopyTo(filestream);
+					}
+				}
+
+			}
+
+			return uniqueFileName;
+		}
+
+		[HttpPost]
+		public IActionResult Create(EmployeeCreateViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				string uniqueFileName = ProcessUploadedFile(model);
+				Employee newEmployee = new Employee
+				{
+					Name = model.Name,
+					Email = model.Email,
+					Department = model.Department,
+					PhotoPath = uniqueFileName,
+				};
+				_employeeRepository.Add(newEmployee);
+				return RedirectToAction("details", new { id = newEmployee.Id });
+			}
+			return View();
+		}
+
+	}
 
 }
